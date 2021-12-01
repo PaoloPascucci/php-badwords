b24f840694bdb91ad25f85018fea59e7313adf53
PaoloPascucci <pascuccip9@gmail.com>
2021-12-01 17:16:25 +0100

    Add base PHP Structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$Usertext="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis molestiae magnam maxime doloribus aliquam laudantium, eaque expedita, fugit maiores iure repellat, voluptatum hic consequuntur? Ipsa explicabo rerum maxime soluta perferendis."
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BADWORD</title>
+</head>
+<body>
+    <p>
+        <?php
+        echo $Usertext;
+        ?>
+    </p>
+</body>
+</html>
